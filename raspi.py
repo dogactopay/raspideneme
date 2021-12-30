340b15e7a4b73d3136bc4f07bd26ec75ee69f438
dogactopay <dogac.topay@gmail.com>
2021-12-30 13:15:14 +0300

    first commit

--- raspi.py
+++ raspi.py
@@ -1,10 +1,7 @@
-from gpiozero import LED
-from time import sleep
-
-led = LED(12)
-
-while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
+ 
+RELAIS_1_GPIO = 17
+GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
+GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
+GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
